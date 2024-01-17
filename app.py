3e26eab6fec31b126aeb688234f1094023a0700a
vishwaschaudhary <vishwaschaudhary>
2024-01-18 05:03:45 +0530

    regressor mostly completed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="TweakML",
                    layout="wide")
 
-
+#setting title 
 st.write("""
 # TweakML
 
@@ -14,32 +14,73 @@ The perfect place to easily train different Machine Learning models according to
 
 """)
 
-task=st.radio("Select any one option:",options=["Regression","Binary Classification","Multi-class Classification"])
+#asking the user about the kind of task he wants 
+task=st.radio("Select any one option:",options=["Regression","Binary Classification","Multi-class Classification"],key="task",index=0)
+#giving a dummy dataset to uploaded_file so that the interpreter executes the code
+ 
+#taking data from the user or asking him to use sample data
 
-st.markdown('#### Upload your CSV data')
-uploaded_file = st.file_uploader("Please upload preprocessed data with last column as target variable", type=["csv"])
+# uploaded_file 
+# if uploaded_file is None:
 
-def after_data_input(df):
+
+#storing this uploaded data on once
+
+
+st.write("#### Would you like to upload your dataset or proceed with example dataset:")
+user_choice=st.radio("choice",options=["Upload dataset","Example dataset"])
+if user_choice=="Example dataset":
+    if task=="Regression":
+        uploaded_file="example_data/Real estate.csv"
+    elif task=="Binary Classification":
+        uploaded_file="example_data/binary classification.csv"
+    else:
+        uploaded_file="example_data/Multiclass_classification.csv"
+else: 
+    uploaded_file= st.file_uploader("Please upload preprocessed data", type=["csv"])
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+    st.markdown('**1.1. Glimpse of dataset**')
+    st.write(df.head(5))
+    target=st.selectbox("Select the target variable",options=list(df.columns))
+    X=df.drop(target,axis=1)
+    y=df[target]
+
+    st.markdown('**1.2. Data splits**')
+    st.write('Training set')
+    st.info(X.shape)
+    st.write("Test set")
+    st.info(y.shape)
+
+    st.markdown('**1.3. Variable details**:')
+    st.write("X variable")
+    st.info(list(X.columns))
+    st.write("y variable")
+    st.info(y.name)
+
     if task=="Regression":
         st.write("Choose your model to train:")
-        algorithm=st.radio("",options=["LinearRegression","SVR","DecisionTreeRegressor","RandomForestRegressor"])
+        
+
+        algorithm=st.selectbox("label",options=("LinearRegression","SVR","DecisionTreeRegressor","RandomForestRegressor"),label_visibility="hidden",key="algorithm")
+        print(algorithm)
         
         if algorithm=="LinearRegression":
             from models.Regression import linearregression
-            linearregression(df)
+            linearregression(X,y)
         elif algorithm=="SVR":
             from models.Regression import svr
             svr(df)
         elif algorithm=="DecisionTreeRegressor":
             from models.Regression import decisiontreeregressor
             decisiontreeregressor(df)
-        else:
+        elif algorithm=="RandomForestRegressor":
             from models.Regression import randomforestregressor
-            randomforestregressor(df)
+            randomforestregressor(X,y)
 
     elif task=="Binary Classification":
         st.write("Choose your model to train:")
-        algorithm=st.radio("",options=["LogisticRegression","SVC","DecisionTreeClassifier","RandomForestClassifier"])
+        algorithm=st.radio("label",options=["LogisticRegression","SVC","DecisionTreeClassifier","RandomForestClassifier"],label_visibility="hidden")
         
         if algorithm=="LogisticRegression":
             from models.Regression import logisticregression
@@ -53,22 +94,20 @@ def after_data_input(df):
         else:
             from models.Regression import randomforestclassifier
             randomforestclassifier(df)
+    else:
+        st.write("code not complete for multiclass classification")
+
+
+
+
+
+
 
     
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.markdown('**1.1. Glimpse of dataset**')
-    st.write(df.head(5))
-    after_data_input(df)
+    
     
 
-else:
-    if st.button('Press to use Example Dataset'):
-        df = pd.read_csv("example_data/Real estate.csv")
-        st.markdown('**1.1. Glimpse of dataset**')
-        st.write(df.head(5))
-        after_data_input(df)
+
         
 
 
--- models/Regression.py
+++ models/Regression.py
@@ -1,36 +1,91 @@
 import streamlit as st
 from sklearn.model_selection import train_test_split
 
-def linearregression(df):
+def linearregression(X,y):
+    from sklearn.linear_model import LinearRegression
+    #parameter selection form
+    with st.form("Linear regression form"):
+        col1,col2=st.columns(2)
+        p_fit_intercept=col1.radio("`fit_intercept`",[True,False])
+        p_copy_X=col2.radio("`copy_X`",[True,False])
+        p_n_jobs=col1.slider("`n_jobs`",-1,10,1,1)
+        p_positive=col2.radio("`positive`",[False,True])
+        param_train_size=col1.slider("`train_size`",10,90,70,10)
+        x=st.form_submit_button("Train model")
+        if x:
+            X_train,X_test,y_train,y_test=train_test_split(X,y,train_size=param_train_size)
+            lr=LinearRegression(fit_intercept=p_fit_intercept,
+                                copy_X=p_copy_X,
+                                n_jobs=p_n_jobs,
+                                positive=p_positive)
+            lr.fit(X_train,y_train)
+            y_pred_train=lr.predict(X_train)
+            y_pred_test=lr.predict(X_test)
+            from metrics import regression_metrics
+
+            st.subheader('2. Model Performance')
+            st.markdown('**2.1. Training set**')
+            regression_metrics(y_train,y_pred_train)
+            st.markdown("**2.2. Test set**")
+            regression_metrics(y_test, y_pred_test)
+
+            st.subheader('3. Model Parameters')
+            st.write(lr.get_params())
+
+
+
+
+def svr(X,y):
+    from sklearn.svm import SVR
+    
+    with st.form("SVR parameter form"):
+        col1,col2=st.columns(2)
+        p_kernel=col1.selectbox("`kernel`",['rbf','linear','poly','sigmoid','precomputed'])
+        if p_kernel=="poly":
+            p_degree=col1.slider("`degree`",1,10,3,1)
+        p_tol=col2.slider("`tol`",0.0,2.0,0.0,0.1)
+        p_C=col1.slider("`C`",0.0,10.0,1.0,0.2)
+        p_epsilon=col2.slider("`epsilon`",0.0,3,0.1,0.1)
+        param_train_size=col1.slider("`train_size`",10,90,70,10)      
+        x=st.form_submit_button("Train model")
+    if x:
+        X_train,X_test,y_train,y_test=train_test_split(X,y,train_size=param_train_size)
+        svr=SVR(kernel=p_kernel,
+                degree=p_degree,
+                tol=p_tol,
+                C=p_C,
+                epsilon=p_epsilon)
+        svr.fit(X_train,y_train)
+        y_pred_train=svr.predict(X_train)
+        y_pred_test=svr.predict(X_test)
+        from metrics import regression_metrics
+
+        st.subheader('2. Model Performance')
+        st.markdown('**2.1. Training set**')
+        regression_metrics(y_train,y_pred_train)
+        st.markdown("**2.2. Test set**")
+        regression_metrics(y_test, y_pred_test)
+
+        st.subheader('3. Model Parameters')
+        st.write(svr.get_params())
+
+
+
+
+        
+
+def decisiontreeregressor(X,y):
+    from sklearn.tree import DecisionTreeRegressor
     pass
 
-def svr(df):
-    pass
-
-def decisiontreeregressor(df):
-    pass
 
 
-
-def randomforestregressor(df):
+def randomforestregressor(X,y):
     from sklearn.ensemble import RandomForestRegressor
-    X=df.iloc[:,:-1]
-    y=df.iloc[:,-1]
-
-    st.markdown('**1.2. Data splits**')
-    st.write('Training set')
-    st.info(X.shape)
-    st.write("Test set")
-    st.info(y.shape)
-
-    st.markdown('**1.3. Variable details**:')
-    st.write("X variable")
-    st.info(list(X.columns))
-    st.write("y variable")
-    st.info(y.name)
+    
 
     #parameter selection form:
-    with st.form("Parameter form"):
+    with st.form("Tune your Parameters"):
         col1,col2=st.columns(2)
         parameter_n_estimators=col1.slider("`n_estimators`",10,500,100,10)
         parameter_max_depth=col2.slider("`max_depth`",1,150,5,2)
@@ -44,38 +99,40 @@ def randomforestregressor(df):
         parameter_criterion=col2.selectbox("`criterion`",["squared_error", "absolute_error", "friedman_mse", "poisson"])
         parameter_min_samples_split=col1.slider("`min_samples_split`",1,100,2,1)
         parameter_min_samples_leaf=col2.slider("`min_samples_leaf`",1,10,1,1)
-        parameter_bootstrap=col1.selectbox("`bootsrap`",[True,False])
+        parameter_bootstrap=col1.selectbox("`bootsrap`",[False,True])
         if parameter_bootstrap:
             parameter_oob_score=col1.selectbox("`oob_score`",[True,False])
         else:
             parameter_oob_score=False
-        param_train_size=col2.slider("`train_size`",10,90,10)
+        param_train_size=col2.slider("`train_size`",10,90,70,10)
+        p_n_jobs=col2.slider("`n_jobs`",-1,10,1,1)
+        x=st.form_submit_button("Train model")
 
+    if x:
+        X_train,X_test, y_train,y_test=train_test_split(X,y,train_size=param_train_size)
+        rf = RandomForestRegressor(n_estimators=parameter_n_estimators,
+            max_depth=parameter_max_depth,
+            max_features=parameter_max_features,
+            criterion=parameter_criterion,
+            min_samples_split=parameter_min_samples_split,
+            min_samples_leaf=parameter_min_samples_leaf,
+            bootstrap=parameter_bootstrap,
+            oob_score=parameter_oob_score,
+            n_jobs=p_n_jobs)
+        rf.fit(X_train, y_train)
 
-    X_train,X_test, y_train,y_test=train_test_split(X,y,train_size=param_train_size)
-    rf = RandomForestRegressor(n_estimators=parameter_n_estimators,
-        max_depth=parameter_max_depth,
-        max_features=parameter_max_features,
-        criterion=parameter_criterion,
-        min_samples_split=parameter_min_samples_split,
-        min_samples_leaf=parameter_min_samples_leaf,
-        bootstrap=parameter_bootstrap,
-        oob_score=parameter_oob_score,
-        n_jobs=-1)
-    rf.fit(X_train, y_train)
+        y_pred_train=rf.predict(X_train)
+        y_pred_test=rf.predict(X_test)
+        from metrics import regression_metrics
 
-    y_pred_train=rf.predict(X_train)
-    y_pred_test=rf.predict(X_test)
-    from metrics import regression_metrics
+        st.subheader('2. Model Performance')
+        st.markdown('**2.1. Training set**')
+        regression_metrics(y_train,y_pred_train)
+        st.markdown("**2.2. Test set**")
+        regression_metrics(y_test, y_pred_test)
 
-    st.subheader('2. Model Performance')
-    st.markdown('**2.1. Training set**')
-    regression_metrics(y_train,y_pred_train)
-    st.markdown("**2.2. Test set**")
-    regression_metrics(y_test, y_pred_test)
-
-    st.subheader('3. Model Parameters')
-    st.write(rf.get_params())
+        st.subheader('3. Model Parameters')
+        st.write(rf.get_params())
     
 
     
